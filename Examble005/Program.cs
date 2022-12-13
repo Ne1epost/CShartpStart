202c5ddc96e1c3aee48319af3dc3115b2c23fd1f
Ne1epost <ne1epost@me.com>
2022-12-13 22:41:10 +0300

    Добавили с вариантами IF

---
+++ Examble005/Program.cs
@@ -0,0 +1,13 @@
+﻿// Пример с ответвлениями
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username == "Маша")
+{
+    Console.Write("Привет! Это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
